9b6a839a3590de442b97ddcd044309cda07fbbde
nisiviglia <ns533@njit.edu>
2017-08-10 20:37:59 -0400

    Refactored startStopBtn function

--- src/Header.js
+++ src/Header.js
@@ -6,11 +6,11 @@ class Header extends Component {
         super();
         this.msg = new SpeechSynthesisUtterance();
         this.startStopBtn = this.startStopBtn.bind(this); 
-        this.handleText = this.handleText.bind(this);
+        this.resetBtn = this.resetBtn.bind(this);
+        this.handjeText = this.handleText.bind(this);
         
         this.state = {
             startStop: "Play",
-            paused: 0,
             textarea: "The quick brown fox jumps over the lazy dog."
         }
     }
@@ -18,30 +18,34 @@ class Header extends Component {
     handleText(event) {
         this.setState({textarea: event.target.value});
     }
-    
+   
     startStopBtn(event) {
-        if (this.state.startStop === "Play"){
-           this.setState({startStop: "Stop"}); 
-           if(this.state.paused === 1){ 
-                this.setState({paused: 0})
-                speechSynthesis.resume();
-           }
-           else{
-               this.msg.text = this.state.textarea;
-               speechSynthesis.speak(this.msg);
-           }
+        if(speechSynthesis.paused === false 
+                && speechSynthesis.speaking === false){
+            this.setState({startStop: "Pause"}); 
+            this.msg.text = this.state.textarea;
+            speechSynthesis.speak(this.msg);
         } 
-        else {
-           this.setState({startStop: "Play", paused: 1}); 
-           speechSynthesis.pause();
+        else if(speechSynthesis.paused === true){
+            this.setState({startStop: "Pause"}); 
+            speechSynthesis.resume();
         }
+        else if(speechSynthesis.speaking === true){
+            this.setState({startStop: "Resume"}); 
+            speechSynthesis.pause();
+        }
+    }
+
+    resetBtn(event){
+        this.setState({startStop: "Play"}); 
+        speechSynthesis.cancel();
     }
 
     render(props){
         return (
           <div> 
             <ul className="controls">
-                <li><a href="#">Reset</a></li>
+                <li><a href="#" onClick={this.resetBtn}>Reset</a></li>
                 <li><a href="#">Highlight</a></li>
                 <li><a href="#" onClick={this.startStopBtn}>
                     {this.state.startStop}
