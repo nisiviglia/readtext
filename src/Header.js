13a901dbe43ea210a43ae26b08defd57f75211e5
nisiviglia <ns533@njit.edu>
2017-08-15 12:43:41 -0400

    basic highlighting added.

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
 import './Header.css'
-
 class Header extends Component {
     constructor() {
         super();
         this.msg = new SpeechSynthesisUtterance();
         this.startStopBtn = this.startStopBtn.bind(this); 
         this.resetBtn = this.resetBtn.bind(this);
-        this.handjeText = this.handleText.bind(this);
+        this.highlightBtn = this.highlightBtn.bind(this)
+        this.handleText = this.handleText.bind(this);
         
         this.state = {
             startStop: "Play",
-            textarea: "The quick brown fox jumps over the lazy dog."
+            textarea: "the quick brown Fox jumps over the lazy Dog.",
+            highlightedText: "the quick brown <mark>Fox</mark> jumps over the lazy dog.",
+            style: {height: 80, width: 300}
         }
     }
     
-    handleText(event) {
-        this.setState({textarea: event.target.value});
+    setHeight(height) {
+        let newState = Object.assign({}, this.state.style);
+        newState.height = height;
+        this.setState({style: newState})
     }
-   
+    
+    highlightText(text) {
+        text = text 
+            .replace(/\n$/g, '\n\n')
+            .replace(/[A-Z].*?\b/g, '<mark>$&</mark>');
+        return text;
+    }
+
+    handleText(event) {
+        this.setState({
+            textarea: event.target.value,
+            highlightedText: this.highlightText(event.target.value)
+        });
+    }
+
+    highlightBtn(event){
+        console.log("code here") 
+    }
+
     startStopBtn(event) {
         if(speechSynthesis.paused === false 
                 && speechSynthesis.speaking === false){
@@ -37,27 +60,42 @@ class Header extends Component {
     }
 
     resetBtn(event){
-        this.setState({startStop: "Play"}); 
+        this.setState({
+            startStop: "Play",
+            highlightedText: null
+        }); 
         speechSynthesis.cancel();
+        this.msg = new SpeechSynthesisUtterance();
     }
-
+    
     render(props){
         return (
           <div> 
             <ul className="controls">
-                <li><a href="#" onClick={this.resetBtn}>Reset</a></li>
-                <li><a href="#">Highlight</a></li>
+                <li><a href="#" onClick={this.resetBtn}>
+                    Reset
+                </a></li>
+                <li><a href="#" onClick={this.highlightBtn}>
+                    Highlight
+                </a></li>
                 <li><a href="#" onClick={this.startStopBtn}>
                     {this.state.startStop}
                 </a></li>
             </ul>
-
-            <textarea className="document" 
-                rows="25" 
-                value={this.state.textarea}
-                onChange={this.handleText}
-                autoFocus>
-            </textarea>
+            <div className="container" style={this.state.style}>
+                <div className="backdrop" style={this.state.style}>
+                    <div className="highlights" 
+                        style={this.state.style}
+                        dangerouslySetInnerHTML={{__html: this.state.highlightedText}}
+                    ></div>
+                </div>
+                <TextareaAutosize
+                    className="textArea"
+                    value={this.state.textarea}
+                    onChange={this.handleText}
+                    onHeightChange={(height) => this.setHeight(height)}
+                />
+            </div>
           </div>      
         ); 
     }
